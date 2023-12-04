639eb818f119310ee671861d5e5ecf3aa8811f15
deven3705 <132754274+deven3705@users.noreply.github.com>
2023-12-04 23:42:51 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,9 +1,7 @@
-//uisng inheriantance
+// ************************************************************ Inheritance *************************************************************
 
 #include <iostream>
-
 using namespace std;
-
 class Shape {
 public:
     virtual double getVolume() = 0;
@@ -60,7 +58,9 @@ int main() {
     return 0;
 }
 
-//uisng exception handling
+
+// ************************************************************ Exception Handling *************************************************************
+
 #include <iostream>
 #include <exception>
 
@@ -171,78 +171,10 @@ int main() {
     return 0;
 }
 
-//uisng friend function#include <iostream>
 
-using namespace std;
+// ************************************************************ Virtual Function *************************************************************
 
-class Shape {
-public:
-    virtual double calculateVolume() = 0;
-};
-
-class Cylinder : public Shape {
-private:
-    double radius;
-    double height;
-
-public:
-    Cylinder(double radius, double height) {
-        this->radius = radius;
-        this->height = height;
-    }
-
-    double calculateVolume() override {
-        return 3.14159 * radius * radius * height;
-    }
-};
-
-class Cube : public Shape {
-private:
-    double sideLength;
-
-public:
-    Cube(double sideLength) {
-        this->sideLength = sideLength;
-    }
-
-    double calculateVolume() override {
-        return sideLength * sideLength * sideLength;
-    }
-};
-
-class Cuboid : public Shape {
-private:
-    double length;
-    double width;
-    double height;
-
-public:
-    Cuboid(double length, double width, double height) {
-        this->length = length;
-        this->width = width;
-        this->height = height;
-    }
-
-    double calculateVolume() override {
-        return length * width * height;
-    }
-};
-
-int main() {
-    Cylinder cylinder(5.0, 10.0);
-    Cube cube(6.0);
-    Cuboid cuboid(8.0, 4.0, 3.0);
-
-    cout << "Volume of cylinder: " << cylinder.calculateVolume() << endl;
-    cout << "Volume of cube: " << cube.calculateVolume() << endl;
-    cout << "Volume of cuboid: " << cuboid.calculateVolume() << endl;
-
-    return 0;
-}
-
-//uisng virtual friend
 #include <iostream>
-
 using namespace std;
 
 class Shape {
@@ -310,7 +242,8 @@ int main() {
     return 0;
 }
 
-//using friend function
+
+// ************************************************************ Friend Function *************************************************************
 
 #include <iostream>
 #include <cmath>
