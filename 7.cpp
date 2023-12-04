ab6eefdd6ae7778e0f16de4109975b02190c005b
deven3705 <132754274+deven3705@users.noreply.github.com>
2023-12-04 23:53:18 +0530

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -55,27 +55,88 @@ int main() {
 }
 
 
-//Exception handling
-#include <iostream>
-
+// ************************************************************ Exception Handling *************************************************************
+#include<iostream>
 using namespace std;
 
-int divide(int x, int y) {
-    if (y == 0) {
-        throw exception(); // Throw an exception if y is 0
+class Exp_handling
+{
+private:
+    int age, vehicle;
+    long double income;
+    string city;
+
+public:
+    void getdata()
+    {
+        cout << "Enter the age :" << endl;
+        cin >> age;
+        cout << "Enter total no. of vehicle wheels :" << endl;
+        cin >> vehicle;
+        cout << "Enter the income :" << endl;
+        cin >> income;
+        cout << "Enter the city :" << endl;
+        cin >> city;
     }
 
-    return x / y;
-}
+    void check()
+    {
+        try
+        {
+            if (age < 18 || age > 55)
+            {
+                throw "Age should be between 18-55";
+            }
+        }
+        catch (const char *res)
+        {
+            cerr << res << endl;
+        }
 
-int main() {
-    try {
-        int result = divide(10, 0); // Will throw an exception
-        cout << "Result: " << result << endl;
-    } catch (exception& e) {
-        cout << "Error: Division by zero" << endl;
+        try
+        {
+            if (income < 50000 || income > 100000)
+            {
+                throw "Income should be between 50,000 and 1,00,000";
+            }
+        }
+        catch (const char *res)
+        {
+            cerr << res << endl;
+        }
+
+        try
+        {
+            if (!(city == "Pune" || city == "pune" || city == "Mumbai" || city == "mumbai" || city == "chennai" ||
+                  city == "Chennai" || city == "delhi" || city == "Delhi"))
+            {
+                throw "City should be delhi/pune/mumbai/chennai";
+            }
+        }
+        catch (const char *res)
+        {
+            cerr << res << endl;
+        }
+
+        try
+        {
+            if (vehicle != 4)
+            {
+                throw "User must have 4 wheeler";
+            }
+        }
+        catch (const char *res)
+        {
+            cerr << res << endl;
+        }
     }
+};
 
+int main()
+{
+    Exp_handling a;
+    a.getdata();
+    a.check();
     return 0;
 }
 
