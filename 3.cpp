53e4eeab571491f43f902d00b8ec0631beaf4caf
deven3705 <132754274+deven3705@users.noreply.github.com>
2023-12-04 23:35:38 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,9 +1,9 @@
-//using inheriantance
+// ************************************************************ Inheritance *************************************************************
 
 #include <iostream>
 
-class ExamResults {
-private:
+class Results {
+protected:
     int passedCount = 0;
     int failedCount = 0;
 
@@ -17,7 +17,10 @@ public:
             std::cout << "Invalid input: Result must be 1 (passed) or 2 (failed)." << std::endl;
         }
     }
+};
 
+class ExamResults : public Results {
+public:
     void displaySummary() {
         std::cout << "Summary of Exam Results:" << std::endl;
         std::cout << "Number of students who passed: " << passedCount << std::endl;
@@ -43,7 +46,9 @@ int main() {
 
     return 0;
 }
-//using exception handling
+
+// ************************************************************ Exception Handling *************************************************************
+
 #include <iostream>
 #include <exception>
 
@@ -97,61 +102,10 @@ int main() {
             results.enterTestResult(result);
         } catch (InvalidResultException& e) {
             std::cerr << "Error: " << e.getMessage() << std::endl;
-            
-// using virtual function
 
 
-#include <iostream>
 
-class ExamResultAnalyzer {
-public:
-    virtual void enterTestResult(int result) = 0;
-    virtual void displaySummary() = 0;
-};
-
-class InteractiveExamResultAnalyzer : public ExamResultAnalyzer {
-private:
-    int passedCount = 0;
-    int failedCount = 0;
-
-public:
-    void enterTestResult(int result) override {
-        if (result == 1) {
-            passedCount++;
-        } else if (result == 2) {
-            failedCount++;
-        } else {
-            std::cout << "Invalid input: Result must be 1 (passed) or 2 (failed)." << std::endl;
-        }
-    }
-
-    void displaySummary() override {
-        std::cout << "Summary of Exam Results:" << std::endl;
-        std::cout << "Number of students who passed: " << passedCount << std::endl;
-        std::cout << "Number of students who failed: " << failedCount << std::endl;
-
-        if (passedCount > 8) {
-            std::cout << "Raise tuition." << std::endl;
-        }
-    }
-};
-
-int main() {
-    InteractiveExamResultAnalyzer analyzer;
-
-    for (int i = 0; i < 10; i++) {
-        int result;
-        std::cout << "Enter result (1 for passed, 2 for failed): ";
-        std::cin >> result;
-        analyzer.enterTestResult(result);
-    }
-
-    analyzer.displaySummary();
-
-    return 0;
-}
-
-//using virtual function
+// ************************************************************ Virtual Function *************************************************************
 
 #include <iostream>
 
@@ -202,7 +156,10 @@ int main() {
 
     return 0;
 }
-//using friend function
+
+
+
+// ************************************************************ Friend function *************************************************************
 
 #include <iostream>
 
